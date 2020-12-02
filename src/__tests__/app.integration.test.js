2673b5112c59ebb19f02a8a8ffe9e038cd1038e9
ewrobel-nhs <edyta.wrobel@nhs.net>
2020-12-02 14:50:29 +0000

    [PRMT-1147] Tested registration request endpoints in app integration

--- src/__tests__/app.integration.test.js
+++ src/__tests__/app.integration.test.js
@@ -1,11 +1,13 @@
 import request from 'supertest';
 import app from '../app';
 import { initializeConfig } from '../config';
+import ModelFactory from '../models';
+import { modelName, Status } from '../models/registration-request';
 
 describe('GET /health', () => {
   const config = initializeConfig();
 
-  it('should return 200 and the response from getHealthCheck', done => {
+  it('should return 200 and the response from getHealthCheck', async () => {
     const expectedHealthCheckResponse = {
       version: '1',
       description: 'Health of Repo To GP service',
@@ -19,31 +21,100 @@ describe('GET /health', () => {
       }
     };
 
-    request(app)
+    const res = await request(app)
       .get('/health')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .expect(res => {
-        expect(res.body).toEqual(expectedHealthCheckResponse);
-      })
-      .end(done);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(expectedHealthCheckResponse);
   });
 });
 
 describe('Swagger Documentation', () => {
-  it('GET /swagger - should return a redirect 301 status code and text/html content type response', done => {
-    request(app)
+  it('GET /swagger - should return a redirect 301 status code and text/html content type response', async () => {
+    const res = await request(app)
       .get('/swagger')
-      .expect(301)
-      .expect('Content-Type', 'text/html; charset=UTF-8')
-      .end(done);
+
+    expect(res.statusCode).toBe(301);
   });
 
-  it('GET /swagger/index.html - should return a 200 status code and text/html content type response', done => {
-    request(app)
+  it('GET /swagger/index.html - should return a 200 status code and text/html content type response', async () => {
+    const res = await request(app)
       .get('/swagger/index.html')
-      .expect(200)
-      .expect('Content-Type', 'text/html; charset=UTF-8')
-      .end(done);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe('GET /registration-requests/:conversationId', () => {
+  const config = initializeConfig();
+  const conversationId = '798d3574-635f-447b-8ff0-a41a95d951db';
+  const nhsNumber = '1234567891';
+  const odsCode = 'B12345';
+  const status = Status.REGISTRATION_REQUEST_RECEIVED;
+
+  it('should return registration request info', async () => {
+    const retrievalResponse = {
+      data: {
+        type: 'registration-requests',
+        id: conversationId,
+        attributes: {
+          nhsNumber,
+          odsCode,
+          status
+        }
+      }
+    };
+
+    const RegistrationRequest = ModelFactory.getByName(modelName);
+    await RegistrationRequest.create({
+      conversationId,
+      nhsNumber,
+      status,
+      odsCode
+    });
+
+    const res = await request(app)
+      .get(`/registration-requests/${conversationId}`)
+      .set('Authorization', config.repoToGpAuthKeys)
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(retrievalResponse);
+  });
+
+  it('should return 404 when registration request cannot be found', async () => {
+    const nonExistentConversationId = '941ca257-f88f-499b-8e13-f8a62e7fea7a';
+    const res = await request(app)
+      .get(`/registration-requests/${nonExistentConversationId}`)
+      .set('Authorization', config.repoToGpAuthKeys)
+
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe('POST /registration-requests/', () => {
+  const config = initializeConfig();
+  const conversationId = 'ed354a0d-be71-4026-804a-4674ca6cdd17';
+  const nhsNumber = '1234567890';
+  const odsCode = 'A12345';
+
+  it('should return a 204 status code for correct request', async () => {
+    const body = {
+      data: {
+        type: 'registration-requests',
+        id: conversationId,
+        attributes: {
+          nhsNumber,
+          odsCode
+        }
+      }
+    };
+
+    const res = await request(app)
+      .post(`/registration-requests/`)
+      .set('Authorization', config.repoToGpAuthKeys)
+      .send(body)
+
+    expect(res.header['location']).toEqual(`${config.repoToGpServiceUrl}/deduction-requests/${conversationId}`);
+    expect(res.statusCode).toBe(204);
   });
 });
