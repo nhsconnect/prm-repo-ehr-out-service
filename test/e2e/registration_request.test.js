d8e80b2c8c24b07071cf6fb8d50837601428eafb
Mircea M <mircea.moise@gmail.com>
2020-12-23 15:55:41 +0000

    [PRMT-1407] Added check for registration request status and added patient data for dev environment

--- test/e2e/registration_request.test.js
+++ test/e2e/registration_request.test.js
@@ -12,65 +12,79 @@ function ehrRequestFor(conversationId, nhsNumber, odsCode) {
 }
 
 describe('EMIS registration requests', () => {
-  it('should capture a registration request', async () => {
-    const testData = {
-      dev: {
-        // TODO: dev patients
-      },
-      test: {
-        odsCode: 'A20047',
-        nhsNumber: '9692294900'
+  const RETRY_COUNT = 20;
+  const POLLING_INTERVAL_MS = 500;
+  const TEST_TIMEOUT = 3 * RETRY_COUNT * POLLING_INTERVAL_MS;
+
+  it(
+    'should capture a registration request',
+    async () => {
+      const testData = {
+        dev: {
+          odsCode: 'A91720',
+          nhsNumber: '9692842304'
+        },
+        test: {
+          odsCode: 'A20047',
+          nhsNumber: '9692294900'
+        }
+      };
+      //action: send an EHR request to MHS Adapter inbound
+      const mhsInboundUrl = config.mhsInboundUrl;
+
+      const conversationId = v4();
+      const envData = testData[config.nhsEnvironment];
+      const nhsNumber = envData.nhsNumber;
+      const odsCode = envData.odsCode;
+      const ehrRequest = ehrRequestFor(conversationId, nhsNumber, odsCode);
+
+      const headers = {
+        Soapaction: 'urn:nhs:names:services:gp2gp/RCMR_IN010000UK05',
+        'Content-Type':
+          'multipart/related;charset="UTF-8";type="text/xml";boundary="0adedbcc-ed0f-415d-8091-4e816bf9d86f";start="<ContentRoot>"'
+      };
+
+      await axios.post(mhsInboundUrl, ehrRequest, { headers: headers, adapter }).catch(error => {
+        console.log("MHS can't handle this message so it returns with 500");
+        console.log(error.response);
+      });
+
+      console.log('ConversationId:', conversationId);
+
+      let registrationStatus;
+      const expectedStatus = 'sent_ehr';
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        const registrationDetails = await getRegistrationDetails(conversationId);
+        registrationStatus = registrationDetails.status;
+        console.log(`try: ${i} - status: ${registrationStatus}`);
+        if (registrationStatus === expectedStatus) {
+          break;
+        }
+        await sleep(POLLING_INTERVAL_MS);
       }
-    };
-    //action: send an EHR request to MHS Adapter inbound
-    const mhsInboundUrl = config.mhsInboundUrl;
-    const repoToGpUrl = config.repoToGpServiceUrl;
-    const repoToGpAuthKeys = config.repoToGpAuthKeys;
 
-    const conversationId = v4();
-    const envData = testData[config.nhsEnvironment];
-    const nhsNumber = envData.nhsNumber;
-    const odsCode = envData.odsCode;
-    const ehrRequest = ehrRequestFor(conversationId, nhsNumber, odsCode);
-
-    const headers = {
-      Soapaction: 'urn:nhs:names:services:gp2gp/RCMR_IN010000UK05',
-      'Content-Type':
-        'multipart/related;charset="UTF-8";type="text/xml";boundary="0adedbcc-ed0f-415d-8091-4e816bf9d86f";start="<ContentRoot>"'
-    };
-
-    await axios.post(mhsInboundUrl, ehrRequest, { headers: headers, adapter }).catch(error => {
-      console.log("MHS can't handle this message so it returns with 500");
-      console.log(error.response.data);
-    });
-
-    console.log("ConversationId:", conversationId);
-
-    // //needs a short wait for the message to go all the way through to repo-to-gp
-    // await sleep(500);
-    //
-    // //assertion: an new entry with that conversation id has been created in repo-to-gp
-    // const response = await axios.get(repoToGpUrl, {
-    //   headers: { Authorization: repoToGpAuthKeys },
-    //   adapter
-    // });
-    //
-    // const expectedData = {
-    //   data: {
-    //     id: conversationId,
-    //     type: 'registration-requests',
-    //     attributes: {
-    //       nhsNumber,
-    //       odsCode,
-    //       status: 'registration_request_received'
-    //     }
-    //   }
-    // };
-    //
-    // expect(response.data).toEqual(expectedData);
-  });
+      expect(registrationStatus).toEqual(expectedStatus);
+    },
+    TEST_TIMEOUT
+  );
 });
 
-function sleep(ms) {
+const sleep = ms => {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
+};
+
+const getRegistrationDetails = async conversationId => {
+  const repoToGpUrl = `https://${config.nhsEnvironment}.repo-to-gp.patient-deductions.nhs.uk`;
+  const repoToGpAuthKeys = config.repoToGpAuthKeys;
+  const registrationDetailsResp = await axios.get(
+    `${repoToGpUrl}/registration-requests/${conversationId}`,
+    {
+      headers: { Authorization: repoToGpAuthKeys },
+      adapter
+    }
+  );
+  if (registrationDetailsResp.status !== 200) {
+    return {};
+  }
+  return registrationDetailsResp.data.data.attributes;
+};
