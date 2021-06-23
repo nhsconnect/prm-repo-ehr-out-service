06f98bdfa4bf4724aaa6527daa658fad729fbfb0
Jack Slater <jslater@thoughtworks.com>
2021-06-23 10:34:37 +0100

    [PRMT-1733] set IAM authentication enabled on rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -12,6 +12,7 @@ resource "aws_rds_cluster" "repo_to_gp_db_cluster" {
   skip_final_snapshot = true
   storage_encrypted       = true
   kms_key_id              = aws_kms_key.repo_to_gp_key.arn
+  iam_database_authentication_enabled  = true
 
   tags = {
     CreatedBy   = var.repo_name
