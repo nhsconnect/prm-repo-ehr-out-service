5636e42edad59016b8a7d5ab4e5c951489956919
Jack Slater <jslater@thoughtworks.com>
2021-06-24 11:17:54 +0100

    [PRMT-1733] created migration role and ran in new terraform plan for db-roles

---
+++ terraform-db-roles/postgres-roles.tf
@@ -0,0 +1,87 @@
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+resource "postgresql_role" "migration_role" {
+  name     = "migration_role"
+}
+
+resource "postgresql_grant" "migration_role_schema_usage_grant" {
+  database    = var.db_name
+  role        = postgresql_role.migration_role.name
+  schema      = "public"
+  object_type = "schema"
+  privileges  = ["USAGE"]
+}
+
+resource "postgresql_grant" "migration_role_table_create_grant" {
+  database    = var.db_name
+  role        = postgresql_role.migration_role.name
+  schema      = "public"
+  object_type = "table"
+  privileges  = ["CREATE"]
+}
+
+resource "postgresql_role" "migration_user" {
+  name     = "migration_user"
+  login    = true
+}
+
+resource "postgresql_grant_role" "migration_user_rds_iam_grant" {
+  role              = postgresql_role.migration_user.name
+  grant_role        = "rds_iam"
+}
+
+resource "postgresql_grant_role" "migration_user_migration_role_grant" {
+  role              = postgresql_role.migration_user.name
+  grant_role        = postgresql_role.migration_role.name
+}
+
+data "aws_iam_policy_document" "ec2-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "ec2.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "db_migration_role" {
+  name               = "${var.environment}-${var.component_name}-DbMigrationRole"
+  assume_role_policy = data.aws_iam_policy_document.ec2-assume-role-policy.json
+  description        = "DbMigration role to migrate db in the pipeline"
+
+  tags = {
+    Environment = var.environment
+    CreatedBy= var.repo_name
+  }
+}
+
+data "aws_iam_policy_document" "db_migration_user_policy_doc" {
+  statement {
+    actions = [
+      "rds-db:connect"
+    ]
+
+    resources = [
+      "arn:aws:rds-db:${var.region}:${local.account_id}:dbuser:${data.aws_ssm_parameter.db_cluster_resource_id.value}/${postgresql_role.migration_user.name}"
+    ]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "db_migration_user_policy" {
+  name   = "${var.environment}-${var.component_name}-db_migration_user"
+  policy = data.aws_iam_policy_document.db_migration_user_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "db_migration_user_policy_attach" {
+  role       = aws_iam_role.db_migration_role.name
+  policy_arn = aws_iam_policy.db_migration_user_policy.arn
+}
+
---
+++ terraform-db-roles/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "repo_name" {
+  type = string
+  default = "prm-deductions-repo-to-gp"
+}
+
+variable "db_name" {
+  type = string
+  default = "repotogpdb"
+}
+
+variable "db_port" {
+  type = string
+  default = "5432"
+}
+
+variable "component_name" {
+  type = string
+  default = "prm-deductions-repo-to-gp"
+}
+
+variable "environment" {}
+variable "db_host" {}
+variable "db_username" {}
+variable "db_password" {}
