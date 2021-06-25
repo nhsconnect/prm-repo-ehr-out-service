cf9053143bf9952a196b546f18268f89b15cbbc8
Oktawia Kata <oktawia.kata1@nhs.net>
2021-06-25 14:45:41 +0100

    [PRMT-1733] Moved back DB migrations on docker startup (required by docker tests)

--- scripts/run-server-with-db.sh
+++ scripts/run-server-with-db.sh
@@ -5,6 +5,7 @@
 
 #Structured logging
 NHS_SERVICE=repo-to-gp
+
 timestamp() {
   date +"%Y-%m-%d %H:%M:%S"
 }
@@ -12,19 +13,8 @@ function jsonPrettify {
   echo "{message: $1, level: $2, timestamp: `timestamp`, service: ${NHS_SERVICE}, environment: ${NHS_ENVIRONMENT} } "
 }
 
-DB_CONNECTION_TIMEOUT=30
-
-count=0
-while ! pg_isready -h ${DATABASE_HOST}; do
-  jsonPrettify "Waiting for ${DATABASE_HOST}:5432" INFO
-  sleep 1
-  ((count++))
-  if [ "${DB_CONNECTION_TIMEOUT}" -le $count ]; then
-    jsonPrettify "Timed-out waiting for DB connection at ${DATABASE_HOST}:5432" WARN
-    exit 5
-  fi
-done
-set -e
+jsonPrettify "Running DB migrations" INFO
+exec ./scripts/migrate-db.sh
 
 jsonPrettify "Loading API Keys" INFO
 source ./scripts/load-api-keys.sh
