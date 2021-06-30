429e691e259f86249829af3d81db39d7bdf7eb9e
Oktawia Kata <oktawia.kata1@nhs.net>
2021-06-30 16:26:43 +0100

    [PRMT-1733] Improved databse config

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,18 @@
 const use_ssl = process.env.USE_SSL_FOR_DB === 'true';
+
 const databaseConfig = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
   dialect: 'postgres',
-  logging: true,
-  ssl: use_ssl,
-  native: use_ssl,
-  dialectOptions: {
-    ssl: use_ssl ? 'require' : 'false'
-  }
+  logging: true
 };
+
+if (use_ssl) {
+  databaseConfig.ssl = use_ssl;
+  databaseConfig.native = use_ssl;
+  databaseConfig.dialectOptions = { ssl: 'require' };
+}
 
 module.exports = databaseConfig;
