cc0d6eba76d7ea577f36576aff6a247438f9c3a6
Jack Slater <jslater@thoughtworks.com>
2021-06-30 10:42:56 +0100

    [PRMT-1733] set db connection logging to true

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const databaseConfig = {
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
   dialect: 'postgres',
-  logging: false
+  logging: true
 };
 
 module.exports = databaseConfig;
