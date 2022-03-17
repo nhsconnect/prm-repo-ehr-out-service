8572d112f971d03caad5fb69a445bdd5c328dfec
Dan Moore <dan.moore1@nhs.net>
2022-03-17 16:51:27 +0000

    Revert "[PRMT-2599] Remove in-app migrations, run before deploy in pipeline"
    This reverts commit 2668de4ec3b9486e3abc636df532b4b3608cdfd9.

--- scripts/run-server-with-db.sh
+++ scripts/run-server-with-db.sh
@@ -13,6 +13,9 @@ function jsonPrettify {
   echo "{message: $1, level: $2, timestamp: `timestamp`, service: ${NHS_SERVICE}, environment: ${NHS_ENVIRONMENT} } "
 }
 
+jsonPrettify "Running DB migrations" INFO
+source ./scripts/migrate-db.sh
+
 jsonPrettify "Loading API Keys" INFO
 source ./scripts/load-api-keys.sh
 
