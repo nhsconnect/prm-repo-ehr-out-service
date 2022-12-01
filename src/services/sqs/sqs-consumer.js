69048fafb0b6f84e0db24c57f9293e006a2df2d1
syedsadiqali <syed.ali69@nhs.net>
2022-12-01 18:13:17 +0000

    [PRMT-2933] Added sqs parameters

--- src/services/sqs/sqs-consumer.js
+++ src/services/sqs/sqs-consumer.js
@@ -4,9 +4,17 @@ import { parse } from '../parser/sqs-incoming-message-parser.js';
 const sqsClient = new SQSClient({ region: process.env.AWS_DEFAULT_REGION || 'eu-west-2' });
 const ehrOutIncoming = process.env.SQS_EHR_OUT_INCOMING_QUEUE_URL;
 
+const params = {
+  AttributeNames: ["SentTimestamp"],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: ["All"],
+  QueueUrl: ehrOutIncoming,
+  WaitTimeSeconds: 20,
+};
+
 export const run = async () => {
   try {
-    const data = await sqsClient.send(new ReceiveMessageCommand({ QueueUrl: ehrOutIncoming }));
+    const data = await sqsClient.send(new ReceiveMessageCommand(params));
     parse();
     return data;
   } catch (err) {
