d618badf1f8bebf5e3f0f98f693c245f1ffb238e
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2022-12-09 15:28:01 +0000

    [PRMT-2993] Update unit test to assert function call count

--- src/services/sqs/__test__/sqs-consumer.test.js
+++ src/services/sqs/__test__/sqs-consumer.test.js
@@ -1,51 +1,51 @@
 import { startSqsConsumer } from '../sqs-consumer.js';
 import { parse } from '../../parser/sqs-incoming-message-parser';
 import { SQSClient } from '@aws-sdk/client-sqs';
+import { logError } from '../../../middleware/logging';
 
 jest.mock('../../parser/sqs-incoming-message-parser');
+jest.mock('../../../middleware/logging');
 jest.mock('@aws-sdk/client-sqs');
-jest.useFakeTimers();
-jest.spyOn(global, 'setTimeout');
 
 describe('sqs consumer', () => {
   it('read message from the queue and invoke parser', async () => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
     const sqsSendMessageSpy = jest.spyOn(SQSClient.prototype, 'send');
+    const message1 = { key: 'this is a test message 1' };
+    const message2 = { key: 'this is a test message 2' };
+    const errorMessage = 'test error message';
+
     sqsSendMessageSpy
       .mockReturnValueOnce(
         new Promise(resolve => {
-          resolve({ key: 'this is a test message 1' });
+          resolve(message1);
+        })
+      )
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => {
+          reject(errorMessage);
         })
       )
       .mockReturnValueOnce(
         new Promise(resolve => {
-          resolve({ key: 'this is a test message 2' });
+          resolve(message2);
         })
       );
 
     await startSqsConsumer();
+    await jest.advanceTimersByTime(210);
 
-    expect(sqsSendMessageSpy).toHaveBeenCalled();
-    expect(parse).toHaveBeenCalled();
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 100);
-    jest.advanceTimersByTime(110);
-    expect(sqsSendMessageSpy).toHaveBeenCalledTimes(2);
-    expect(setTimeout).toHaveBeenCalledTimes(2);
-    // TODO fix expected calls to parse to 2
-    expect(parse).toHaveBeenCalledTimes(1);
-  });
-
-  it('handle error when reading message from the queue', async () => {
-    const message = 'test error message';
-    const sqsSendMessageSpy = jest.spyOn(SQSClient.prototype, 'send');
-    sqsSendMessageSpy.mockReturnValue(
-      new Promise((resolve, reject) => {
-        reject(message);
-      })
+    expect(setTimeout).toHaveBeenCalledTimes(3);
+    expect(sqsSendMessageSpy).toHaveBeenCalledTimes(3);
+    expect(parse).toHaveBeenNthCalledWith(1, message1);
+    expect(parse).toHaveBeenNthCalledWith(2, message2);
+    await expect(parse).toHaveBeenCalledTimes(2);
+    await expect(logError).toHaveBeenCalledTimes(1);
+    expect(logError).toHaveBeenCalledWith(
+      'Error reading from EHR out incoming queue',
+      errorMessage
     );
-
-    await startSqsConsumer();
-
-    expect(sqsSendMessageSpy).toHaveBeenCalled();
-    expect(parse).not.toHaveBeenCalled();
   });
 });
