09d54018125bdcf0785a2226acd740aea8ee0518
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-12-13 11:10:09 +0000

    [PRMT-2993] Made localstack up and running.
    Co-authored-by: Shashin Dayanand <shashin.dayanand@thoughtworks.com>
    Co-authored-by: Jessy Yeh <jessy.yeh@thoughtworks.com>

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 export const config = {
   nhsEnvironment: process.env.NHS_ENVIRONMENT,
   repoToGpUrl: process.env.REPO_TO_GP_URL,
-  localstackEndpointUrl: 'http://localhost:4566',
+  localstackEndpointUrl: process.env.LOCALSTACK_URL,
   region: process.env.AWS_DEFAULT_REGION || 'eu-west-2',
   awsAccountNo: '000000000000',
   SQS_EHR_OUT_INCOMING_QUEUE_URL: 'test-ehr-out-service-incoming'
