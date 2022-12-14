bb3622a1bd41d6d32c11573759033186f334a9ce
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2022-12-14 16:12:46 +0000

    [PRMT-2993] Parse ebXml from ehr request message
    Co-authored-by: “shruthic-nhs” <shruthi.chandekar1@nhs.net>

--- src/services/parser/__test__/sqs-incoming-message-parser.test.js
+++ src/services/parser/__test__/sqs-incoming-message-parser.test.js
@@ -1,29 +1,18 @@
-import { v4 } from 'uuid';
 import { parse } from '../sqs-incoming-message-parser';
-//Interaction ID, Conversation ID, NHS Number, ODS Code, EHR Request ID
-const interactionId = v4();
-const conversationId = v4();
-const ehrRequestId = v4();
-const nhsNumber = '1234567891';
-const odsCode = 'TEST123';
+import { ehrRequest } from './data/RCMR_IN010000UK05';
+import expect from 'expect';
 
-const incomingMessageBody = {
-  data: {
-    // type: 'ehr-out-requests',
-    id: conversationId,
-    attributes: {
-      interactionId,
-      ehrRequestId,
-      nhsNumber,
-      odsCode
-    }
-  }
+const expectedParsedMessage = {
+  interactionId: 'RCMR_IN010000UK05',
+  conversationId: '17a757f2-f4d2-444e-a246-9cb77bef7f22',
+  ehrRequestId: 'FFFB3C70-0BCC-4D9E-A441-7E9C41A897AA',
+  nhsNumber: '9692842304',
+  odsCode: 'A91720'
 };
 
 describe('Parse the incoming message from the ehr-out-incoming-queue', () => {
-  xit('should successfully parse the incoming message', () => {
-    let parsedMessage = parse(incomingMessageBody);
-    console.log(parsedMessage);
-    // expect(parsedMessage.nhsNumber).toBe(nhsNumber);
+  it('should successfully parse the incoming message', async () => {
+    let parsedMessage = await parse(ehrRequest);
+    await expect(parsedMessage.interactionId).toBe(expectedParsedMessage.interactionId);
   });
 });
--- src/services/parser/sqs-incoming-message-parser.js
+++ src/services/parser/sqs-incoming-message-parser.js
@@ -1,12 +1,24 @@
-// import { logInfo } from '../../middleware/logging';
+import { XmlParser } from './xml-parser/xml-parser';
+import { logInfo } from '../../middleware/logging';
 
-export const parse = async () => {
-  // logInfo('Trying to parse ehr-out-service-incoming event');
-  //
-  // const {
-  //   id: conversationId,
-  //   attributes: { interactionId, ehrRequestId, nhsNumber, odsCode }
-  // } = await JSON.parse(message);
-  //
-  // return { conversationId, interactionId, ehrRequestId, nhsNumber, odsCode };
+export const parse = async messageBody => {
+  logInfo('Trying to parse ehr-out-service-incoming event');
+  try {
+    let ebXmlParser = await getEbXmlParser(messageBody.ebXML);
+    const interactionId = ebXmlParser['data']['Envelope']['Header']['MessageHeader']['Action'];
+    // const test = ebXmlParser.findFirst('envelope/header/action');
+
+    // determine request type by interaction id
+    // parse the payload
+
+    return {
+      interactionId: interactionId
+    };
+  } catch (e) {
+    // ToDo Handle errors
+  }
+};
+
+const getEbXmlParser = async ebXml => {
+  return await new XmlParser().parse(ebXml);
 };
