e766c4e59c76645252f0f381725a59973fa29196
Jessy Yeh <jessy.yeh@thoughtworks.com>
2022-12-16 16:03:02 +0000

    [PRMT-2993] WIP handler for ehr request
    Co-authored-by: Shruthi Chandekar <shruthi.chandekar1@nhs.net>

---
+++ src/services/handler/__test__/ehrRequestHandler.test.js
@@ -0,0 +1,5 @@
+import ehrRequestHandler from '../ehrRequestHandler';
+
+describe('ehrRequestHandler', () => {
+  it('should handle the ehr request and call the registration request', () => {});
+});
--- src/services/parser/sqs-incoming-message-parser.js
+++ src/services/parser/sqs-incoming-message-parser.js
@@ -1,6 +1,7 @@
-import { logError, logInfo } from '../../middleware/logging';
+import { logError, logInfo, logWarning } from '../../middleware/logging';
 import { extractEbXmlData } from './extract-eb-xml-data';
 import { extractPayloadData } from './extract-payload-data';
+import { INTERACTION_IDS } from '../../constants/interaction-ids';
 
 export const parse = async messageBody => {
   logInfo('Parsing ehr-out-service-incoming event');
@@ -9,11 +10,21 @@ export const parse = async messageBody => {
     const { interactionId, conversationId } = await extractEbXmlData(JSON.parse(messageBody).ebXML);
     logInfo('Successfully parsed ebXML');
 
-    const { ehrRequestId, nhsNumber, odsCode } = await extractPayloadData(
-      JSON.parse(messageBody).payload,
-      interactionId
-    );
-    logInfo(`Successfully parsed payload`);
+    let ehrRequestId = undefined,
+      nhsNumber = undefined,
+      odsCode = undefined;
+
+    //we can take out this if statement
+    if (interactionId === INTERACTION_IDS.EHR_REQUEST_INTERACTION_ID) {
+      [ehrRequestId, nhsNumber, odsCode] = await extractPayloadData(
+        JSON.parse(messageBody).payload,
+        interactionId
+      );
+      logInfo(`Successfully parsed payload`);
+    } else {
+      logWarning('Invalid interaction id ' + interactionId);
+    }
+
     logInfo('Successfully parsed ehr-out-service-incoming event');
 
     return {
