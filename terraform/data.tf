e7188ee96785ff130e0dd79ff4b88f517f515f32
Rosie Davidson <rosie.amphlett1@nhs.net>
2023-01-03 15:28:21 +0000

    [PRMT-2721] Subscribed ehr-out-service-incoming sqs queue to the ehr-in-unhandled sns topic

--- terraform/data.tf
+++ terraform/data.tf
@@ -51,3 +51,7 @@ data "aws_ssm_parameter" "vpn_sg_id" {
 data "aws_ssm_parameter" "gocd_sg_id" {
   name = "/repo/${var.environment}/user-input/external/gocd-agent-sg-id"
 }
+
+data "aws_ssm_parameter" "ehr_in_unhandled_sns_topic_arn" {
+  name = "/repo/${var.environment}/output/ehr-transfer-service/ehr-in-unhandled-sns-topic-arn"
+}
---
+++ terraform/sns-topic-subscription.tf
@@ -0,0 +1,6 @@
+resource "aws_sns_topic_subscription" "ehr_in_unhandled_topic" {
+  protocol             = "sqs"
+  raw_message_delivery = true
+  topic_arn            = data.aws_ssm_parameter.ehr_in_unhandled_sns_topic_arn.value
+  endpoint             = aws_sqs_queue.ehr-out-service-incoming.arn
+}
