7e0a0e0cf17cf875fb50d0ebe96a9b56ee67f42e
Dan Moore <dan.moore1@nhs.net>
2023-01-25 08:20:49 +0000

    [PRMT-2978] Default config to prod-like, override by exception

--- terraform/test.tfvars
+++ terraform/test.tfvars
@@ -1,10 +1,9 @@
 environment    = "test"
-component_name = "repo-to-gp"
-dns_name       = "repo-to-gp"
-task_cpu    = 256
-task_memory = 512
-port        = 3000
 service_desired_count = "2"
-alb_deregistration_delay = 15
+
+log_level = "debug"
+
 grant_access_through_vpn = true
 enable_rds_cluster_deletion_protection = false
+
+db_instance_number = 1
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,21 +14,41 @@ variable "db_name" {
 }
 
 variable "environment" {}
-variable "component_name" {}
-variable "dns_name" {}
+variable "component_name" {
+  default = "repo-to-gp"
+}
+variable "dns_name" {
+  default = "repo-to-gp"
+}
 variable "task_image_tag" {}
-variable "task_cpu" {}
-variable "task_memory" {}
-variable "port" {}
-variable "service_desired_count" {}
-variable "alb_deregistration_delay" {}
+variable "task_cpu" {
+  default = 256
+}
+variable "task_memory" {
+  default = 512
+}
+variable "port" {
+  default = 3000
+}
+variable "service_desired_count" {
+  default = "3"
+}
+variable "alb_deregistration_delay" {
+  default = 15
+}
 variable "application_database_user" {
   default = "application_user"
   description = "Needs to match with the user created in db-roles tf plan"
 }
-variable "grant_access_through_vpn" {}
-variable "allow_vpn_to_ecs_tasks" { default=false }
-variable "enable_rds_cluster_deletion_protection" {}
+variable "grant_access_through_vpn" {
+  default = false
+}
+variable "allow_vpn_to_ecs_tasks" {
+  default = false
+}
+variable "enable_rds_cluster_deletion_protection" {
+  default = true
+}
 
 variable "threshold_approx_age_oldest_message" {
   default = "300"
@@ -40,9 +60,9 @@ variable "period_of_age_of_message_metric" {
 
 variable "log_level" {
   type = string
-  default = "debug"
+  default = "info"
 }
 
 variable "db_instance_number" {
-  default = 1
+  default = 3
 }
