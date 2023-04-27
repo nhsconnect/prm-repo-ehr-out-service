eeb612fb2dcb5381f3b932c8a4345ed689a1c6aa
martin-nhs <martin.taylor22@nhs.net>
2023-04-27 11:26:56 +0100

    [PRMT-3162] Added more coverage to integration-test.utilities.test.js

--- src/__tests__/utilities/__tests__/integration-test.utilities.test.js
+++ src/__tests__/utilities/__tests__/integration-test.utilities.test.js
@@ -1,8 +1,54 @@
 import { readFile, validateMessageEquality } from "../integration-test.utilities";
 import expect from "expect";
+import { FileReadError } from "../../../errors/errors";
 
 describe('integration-test-utilities.js', () => {
-  describe('readMessage', () => {});
+  describe('readMessage', () => {
+    it('should read a file successfully if the file name and folder path is correct', () => {
+      // given
+      const fileName = 'RCMR_IN030000UK06';
+      const folders = [
+        'equality-test',
+        'large-ehr',
+        'original'
+      ];
+
+      // when
+      const result = readFile(fileName, ...folders);
+
+      // then
+      expect(result).not.toBe(undefined);
+    });
+
+    it('should throw a FileReadError when the file name is incorrect' , () => {
+      // given
+      const fileName = 'RCMR_IN030000UK10';
+      const folders = [
+        'equality-test',
+        'large-ehr',
+        'original'
+      ];
+
+      // then
+      expect(() => readFile(fileName, ...folders))
+        .toThrow(FileReadError);
+    });
+
+    it('should throw a FileReadError when a folder name is incorrect', () => {
+      // given
+      const fileName = 'RCMR_IN030000UK06';
+      const folders = [
+        'equality-test',
+        'large-ehr',
+        'original',
+        'unknown'
+      ];
+
+      // then
+      expect(() => readFile(fileName, ...folders))
+        .toThrow(FileReadError);
+    });
+  });
 
   describe('validateMessageEquality', () => {
     it('should be true when a large ehr UK06 has no unexpected changes', () => {
