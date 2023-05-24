7e01c61e484f98730058b2bbd21120b7003d16ad
Joe Fong <joe.fong1@nhs.net>
2023-05-24 13:47:16 +0100

    Add logging to trace problem during ehr-out test with FSS

--- src/services/database/message-fragment-repository.js
+++ src/services/database/message-fragment-repository.js
@@ -1,12 +1,20 @@
 import ModelFactory from '../../models';
 import { modelName } from '../../models/message-fragment';
 import { runWithinTransaction } from './helper';
-import { logInfo, logWarning } from "../../middleware/logging";
+import { logError, logInfo, logWarning } from "../../middleware/logging";
 
 const MessageFragment = ModelFactory.getByName(modelName);
 
-export const getMessageFragmentStatusByMessageId = async messageId => {
-  return MessageFragment.findByPk(messageId);
+export const getMessageFragmentStatusByMessageId = messageId => {
+  logInfo(`Getting the status of fragment with message id ${messageId} from database`);
+
+  // TODO: refactor this to a better shape
+  try {
+    return MessageFragment.findByPk(messageId);
+  } catch (error) {
+    logError("Encountered error during database transaction", error)
+    return Promise.resolve(null);
+  }
 };
 
 export const updateMessageFragmentStatus = async (messageId, status) => {
