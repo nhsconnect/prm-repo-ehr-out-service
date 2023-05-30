23604d2e3bf25317e477cdc7fd3ed72e09ab8dff
martin-nhs <martin.taylor22@nhs.net>
2023-05-30 14:53:02 +0100

    [PRMT-2729] Refactored acknowledgement-handler.js

--- src/services/handler/acknowledgement-handler.js
+++ src/services/handler/acknowledgement-handler.js
@@ -1,10 +1,10 @@
-import { parseCommonAcknowledgementFields } from "../parser/acknowledgement-parser";
-import { setCurrentSpanAttributes } from "../../config/tracing";
-import { logError, logInfo } from "../../middleware/logging";
 import {
   parseConversationId
 } from "../parser/parsing-utilities";
+import { logError, logInfo } from "../../middleware/logging";
+import { setCurrentSpanAttributes } from "../../config/tracing";
 import { ACKNOWLEDGEMENT_TYPES } from "../../constants/acknowledgement-types";
+import { parseCommonAcknowledgementFields } from "../parser/acknowledgement-parser";
 
 export const acknowledgementMessageHandler = async message => {
   const conversationId = await parseConversationId(message);
@@ -18,23 +18,6 @@ export const acknowledgementMessageHandler = async message => {
       break;
     case ACKNOWLEDGEMENT_TYPES.NEGATIVE.includes(commonFields.acknowledgementTypeCode):
       logInfo(`NEGATIVE ACKNOWLEDGEMENT RECEIVED IN RESPONSE TO MESSAGE ID ${commonFields.referencedMessageId}`);
-
-      /*
-        fields required for acknowledgement table
-        messageId (UNIQUE TO ACK MESSAGE)
-        conversationId (ASSUMED TO BE THE CONVO ID IN RESPONSE)
-
-        referencedMessageId (MID IN RESPONSE)
-        typeCode
-        RSONS (MAY OR MAY NOT BE PRESENT) (can't recall name array of failure reasons)
-          EITHER:
-            1. acknowledgement -> acknowledgementDetail -> code -> (attr) displayName
-            2.
-       */
-
-
-
-      // Add a new row to the message acknowledgement tracking (TODO) database table.
       break;
     default:
       logError(`ACKNOWLEDGEMENT TYPE ${commonFields.acknowledgementTypeCode} IS UNKNOWN.`);
