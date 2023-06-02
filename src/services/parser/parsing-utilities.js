e056656f370507280014c5e7daad074032394e63
martin-nhs <martin.taylor22@nhs.net>
2023-06-02 13:38:28 +0100

    [PRMT-2729] Added additional functionality to parsing-utilities.js

--- src/services/parser/parsing-utilities.js
+++ src/services/parser/parsing-utilities.js
@@ -24,3 +24,33 @@ export const parseConversationId = async message => {
 
   return parsedFields.conversationId;
 };
+
+export const parseMessageId = async message => {
+  const ebxmlAsJson = await new XmlParser().parse(JSON.parse(message).ebXML);
+
+  const parsedFields = {
+    messageId: ebxmlAsJson['data']['Envelope']['Header']['MessageHeader']['MessageData']['MessageId']
+  };
+
+  validateFieldsHaveSuccessfullyParsed(parsedFields);
+
+  return parsedFields.messageId;
+}
+
+export const extractReferencedFragmentMessageIds = async message => {
+  const ebXmlAsJson = await new XmlParser().parse(JSON.parse(message).ebXML);
+
+  const parsedFields = {
+    reference: ebXmlAsJson['data']['Envelope']['Body']['Manifest']['Reference']
+  }
+
+  validateFieldsHaveSuccessfullyParsed(parsedFields);
+
+  // Ensure we got an array before continue, as parser might yield the field 'Reference'
+  // as an array or single object, depends on how many references in input file.
+  const referencesList = Array.isArray(parsedFields.reference) ? parsedFields.reference : [parsedFields.reference];
+
+  const fragmentReferences = referencesList.filter(reference => reference?.href.startsWith('mid:'));
+
+  return fragmentReferences.map(reference => reference.href.replace('mid:', ''));
+};
