f8cbb414ac0ca907c9de59fcc6bb6cf8efc65bc2
martin-nhs <martin.taylor22@nhs.net>
2023-06-06 10:55:03 +0100

    [PRMT-2729] Refactored continue-request-parser.test.js to reflect JSON parse removal from parsers.

--- src/services/parser/__test__/continue-request-parser.test.js
+++ src/services/parser/__test__/continue-request-parser.test.js
@@ -1,17 +1,20 @@
+import { validateFieldsHaveSuccessfullyParsed } from "../parsing-validation";
 import { parseContinueRequestMessage } from "../continue-request-parser";
 import { readFileSync } from "fs";
 import expect from "expect";
 import path from "path";
-import { validateFieldsHaveSuccessfullyParsed } from "../parsing-validation";
 
 // Mocking
 jest.mock('../parsing-validation');
 
 describe('continue-request-parser.js', () => {
+  // ============ COMMON PROPERTIES ============
+  const exampleContinueRequest = JSON.parse(readFileSync(path.join(__dirname, "data", "continue-requests", "COPC_IN000001UK01"), "utf-8"));
+  // =================== END ===================
+
   it('should parse a continue request successfully', async () => {
     // given
     const odsCode = "M85019";
-    const exampleContinueRequest = readFileSync(path.join(__dirname, "data", "continue-requests", "COPC_IN000001UK01"), "utf-8");
 
     validateFieldsHaveSuccessfullyParsed.mockReturnValueOnce(undefined);
 
