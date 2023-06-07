ba7be2b42a3d3abd5e630b91a99f37f2ca3d04a7
martin-nhs <martin.taylor22@nhs.net>
2023-06-07 16:10:56 +0100

    [PRMT-3405] Created acknowledgements table and migration.

---
+++ src/models/acknowledgements.js
@@ -0,0 +1,51 @@
+import { getParameters } from './parameters';
+
+export const modelName = 'Acknowledgement';
+const tableName = 'acknowledgements';
+
+const model = dataType => ({
+    messageId: {
+        field: 'message_id',
+        type: dataType.UUID,
+        primaryKey: true,
+        allowNull: false
+    },
+    acknowledgementTypeCode: {
+        field: 'acknowledgement_type_code',
+        type: dataType.STRING,
+        allowNull: false
+    },
+    acknowledgementDetail: {
+        field: 'acknowledgement_detail',
+        type: dataType.STRING,
+        allowNull: false
+    },
+    service: {
+        field: 'service',
+        type: dataType.STRING,
+        allowNull: false
+    },
+    referencedMessageId: {
+        field: 'referenced_message_id',
+        type: dataType.STRING,
+        allowNull: false
+    },
+    messageRef: {
+        field: 'message_ref',
+        type: dataType.STRING,
+        allowNull: false
+    },
+    updatedAt: {
+        field: 'updated_at',
+        type: dataType.DATE,
+        allowNull: false
+    },
+    deletedAt: {
+        field: 'deleted_at',
+        type: dataType.DATE
+    }
+});
+
+export default (sequelize, DataTypes) => {
+    return sequelize.define(modelName, model(DataTypes), getParameters(tableName));
+};
--- src/models/models.js
+++ src/models/models.js
@@ -2,5 +2,6 @@ import HealthCheck from './health-check';
 import RegistrationRequest from './registration-request';
 import MessageFragment from './message-fragment';
 import MessageIdReplacement from './message-id-replacement';
+import Acknowledgement from "./acknowledgements";
 
-export { HealthCheck, RegistrationRequest, MessageFragment, MessageIdReplacement };
+export { HealthCheck, RegistrationRequest, MessageFragment, MessageIdReplacement, Acknowledgement };
