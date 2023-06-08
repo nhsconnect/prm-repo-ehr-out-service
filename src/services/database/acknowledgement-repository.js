1ebb5890228588e5ac85027b26b738731a0e08d2
martin-nhs <martin.taylor22@nhs.net>
2023-06-08 11:27:44 +0100

    [PRMT-3405] Added error handling functionality to acknowledgement-repository.js and addressed bugs.

--- src/services/database/acknowledgement-repository.js
+++ src/services/database/acknowledgement-repository.js
@@ -1,13 +1,15 @@
-import ModelFactory from '../../models';
-import { modelName } from '../../models/message-fragment';
 import { AcknowledgementRecordNotFoundError } from "../../errors/errors";
+import { modelName } from '../../models/acknowledgements';
+import { logError } from "../../middleware/logging";
+import ModelFactory from '../../models';
 
 const Acknowledgement = ModelFactory.getByName(modelName);
 
-export const getAcknowledgementByMessageId = messageId => {
-    return Acknowledgement.findByPk(messageId)
+export const getAcknowledgementByMessageId = async messageId => {
+    const acknowledgement = await Acknowledgement.findByPk(messageId)
         .then(acknowledgement => acknowledgement)
-        .catch(error => {
-            throw new AcknowledgementRecordNotFoundError(messageId);
-        });
+        .catch(error => logError(error));
+
+    if(acknowledgement != null) return acknowledgement;
+    else throw new AcknowledgementRecordNotFoundError(messageId);
 };
