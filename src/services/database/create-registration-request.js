0a628e8e2d7b5b1887ba1007879d88ab1d08bcd6
martin-nhs <martin.taylor22@nhs.net>
2023-06-23 14:37:34 +0100

    [PRMT-2730] Added `messageId` to `registration-requests` table.

--- src/services/database/create-registration-request.js
+++ src/services/database/create-registration-request.js
@@ -1,16 +1,17 @@
-import { runWithinTransaction } from './helper';
-import { logError, logInfo } from "../../middleware/logging";
-import ModelFactory from '../../models';
 import { modelName, Status } from '../../models/registration-request';
+import { logError, logInfo } from "../../middleware/logging";
+import { runWithinTransaction } from './helper';
+import ModelFactory from '../../models';
 
 const RegistrationRequest = ModelFactory.getByName(modelName);
 
-export const createRegistrationRequest = (conversationId, nhsNumber, odsCode) =>
+export const createRegistrationRequest = (conversationId, nhsNumber, messageId, odsCode) =>
   runWithinTransaction(transaction =>
     RegistrationRequest.create(
       {
         conversationId,
         nhsNumber,
+        messageId,
         odsCode,
         status: Status.REGISTRATION_REQUEST_RECEIVED
       },
