48a231cc07adfe767334d2ec94cc6fe1ec59b4d8
martin-nhs <martin.taylor22@nhs.net>
2023-06-30 13:53:52 +0100

    [PRMT-2730] Restructured logic within acknowledgement-handler.js to handle integration acknowledgements.

--- src/services/handler/acknowledgement-handler.js
+++ src/services/handler/acknowledgement-handler.js
@@ -4,33 +4,40 @@ import { setCurrentSpanAttributes } from "../../config/tracing";
 import { ACKNOWLEDGEMENT_TYPES } from "../../constants/acknowledgement-types";
 import { parseAcknowledgementMessage } from "../parser/acknowledgement-parser";
 import { createAcknowledgement } from "../database/create-acknowledgement";
-import { registrationRequestExistsWithMessageId } from "../database/registration-request-repository";
+import { sendDeleteRequestToEhrRepo } from "../ehr-repo/delete-ehr";
+import {
+  getNhsNumberByConversationId,
+  registrationRequestExistsWithMessageId
+} from "../database/registration-request-repository";
 
 export const acknowledgementMessageHandler = async message => {
   const conversationId = await parseConversationId(message);
   const parsedAcknowledgementFields = await parseAcknowledgementMessage(message);
-  const typeCode = parsedAcknowledgementFields.acknowledgementTypeCode;
-  const isIntegrationAcknowledgement = await registrationRequestExistsWithMessageId(
-      parsedAcknowledgementFields.messageRef
-  );
+  const { typeCode, messageRef } = parsedAcknowledgementFields;
+  const isIntegrationAcknowledgement = await registrationRequestExistsWithMessageId(messageRef);
+  const nhsNumber = await getNhsNumberByConversationId(conversationId);
 
   setCurrentSpanAttributes({ conversationId });
 
   await createAcknowledgement(parsedAcknowledgementFields);
 
   if (ACKNOWLEDGEMENT_TYPES.POSITIVE.includes(typeCode)) {
-    if (isIntegrationAcknowledgement) handlePositiveIntegrationAcknowledgement();
+    if (isIntegrationAcknowledgement) await handlePositiveIntegrationAcknowledgement(nhsNumber, conversationId);
     else handlePositiveAcknowledgement();
   } else if (ACKNOWLEDGEMENT_TYPES.NEGATIVE.includes(typeCode)) {
-    if (isIntegrationAcknowledgement) handleNegativeIntegrationAcknowledgement()
+    if (isIntegrationAcknowledgement) handleNegativeIntegrationAcknowledgement();
     else handleNegativeAcknowledgement();
   } else {
     logError(`ACKNOWLEDGEMENT TYPE ${typeCode} IS UNKNOWN.`);
   }
 };
 
-const handlePositiveIntegrationAcknowledgement = () => {
-  logInfo(`POSITIVE INTEGRATION ACKNOWLEDGEMENT RECEIVED`);
+const handlePositiveIntegrationAcknowledgement = async (nhsNumber, conversationId) => {
+  const usefulDetails = `for Conversation ID ${conversationId}, and NHS number ${nhsNumber}.`;
+  logInfo(`Positive integration acknowledgement received ${usefulDetails}`);
+  logInfo(`Sending delete request to ehr out repository ${usefulDetails}`);
+
+  await sendDeleteRequestToEhrRepo(nhsNumber, conversationId);
 };
 
 const handleNegativeIntegrationAcknowledgement = () => {
