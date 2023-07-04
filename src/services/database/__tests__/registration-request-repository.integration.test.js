3af4d8fde9a1bfa9b9f422dfb682765b02fd52a4
martin-nhs <martin.taylor22@nhs.net>
2023-07-04 10:17:08 +0100

    [PRMT-2730] Addressed PR comment https://github.com/nhsconnect/prm-repo-ehr-out-service/pull/43#discussion_r1251096151

--- src/services/database/__tests__/registration-request-repository.integration.test.js
+++ src/services/database/__tests__/registration-request-repository.integration.test.js
@@ -1,13 +1,12 @@
-import { v4 as uuid } from 'uuid';
-import ModelFactory from '../../../models';
+import { createRegistrationRequest } from "../create-registration-request";
 import { modelName, Status } from '../../../models/registration-request';
+import { NhsNumberNotFoundError } from "../../../errors/errors";
 import {
   getNhsNumberByConversationId,
   getRegistrationRequestStatusByConversationId, registrationRequestExistsWithMessageId,
   updateRegistrationRequestStatus
 } from '../registration-request-repository';
-import { NhsNumberNotFoundError } from "../../../errors/errors";
-import { createRegistrationRequest } from "../create-registration-request";
+import ModelFactory from '../../../models';
 
 describe('Registration request repository', () => {
   const RegistrationRequest = ModelFactory.getByName(modelName);
@@ -24,7 +23,7 @@ describe('Registration request repository', () => {
   it('should retrieve the registration request by conversation id', async () => {
     // given
     const conversationId = '22a748b2-fef6-412d-b93a-4f6c68f0f8dd';
-    const messageId = uuid();
+    const messageId = 'e4d0dde1-80bb-4c52-a299-90c92fdd5466';
     const odsCode = 'B12345';
     const nhsNumber = '1234567891';
     const status = Status.REGISTRATION_REQUEST_RECEIVED;
@@ -42,7 +41,7 @@ describe('Registration request repository', () => {
 
   it('should return null when it cannot find the registration request', async () => {
     // given
-    const nonExistentConversationId = uuid();
+    const nonExistentConversationId = '9c201efc-28b0-4ea0-ac5b-baf35bd575b8';
 
     // when
     const registrationRequest = await getRegistrationRequestStatusByConversationId(
@@ -56,7 +55,7 @@ describe('Registration request repository', () => {
   it('should change registration request status to invalid_ods_code', async () => {
     // given
     const conversationId = 'e30d008e-0134-479c-bf59-6d4978227617';
-    const messageId = uuid();
+    const messageId = '25bbc875-b5c8-4086-b420-ff759ab52426';
     const nhsNumber = '1234567890';
     const status = Status.INCORRECT_ODS_CODE;
     const odsCode = 'B1234';
@@ -74,8 +73,8 @@ describe('Registration request repository', () => {
   describe('getNhsNumberByConversationId', () => {
     it('should return the nhs number of a registration-request', async () => {
       // given
-      const conversationId = uuid();
-      const messageId = uuid();
+      const conversationId = 'c8549520-b8ba-4ee5-b631-39c513082baa';
+      const messageId = '008ebc80-61f6-485b-b0af-3665343e5d6c';
       const odsCode = 'B12345';
       const nhsNumber = '1234567890'
       const status = Status.REGISTRATION_REQUEST_RECEIVED
@@ -90,7 +89,7 @@ describe('Registration request repository', () => {
 
     it('should throw NHS_NUMBER_NOT_FOUND_ERROR if cannot find the nhs number related to given conversation id', async () => {
       // given
-      const conversationId = uuid();
+      const conversationId = 'a556f067-909a-48c7-8cce-16c5d9564db9';
 
       // when
       await expect(getNhsNumberByConversationId(conversationId))
@@ -102,8 +101,8 @@ describe('Registration request repository', () => {
   describe('registrationRequestExistsWithMessageId', () => {
     it('should return true if a registration request is found, given a valid messageId', async () => {
       // given
-      const conversationId = uuid();
-      const messageId = uuid();
+      const conversationId = 'c511e4dd-f278-4a9d-ad2d-1ac547e9f990';
+      const messageId = 'cb702eef-62e9-4636-a172-2535a0a02508';
       const odsCode = 'B23456';
       const nhsNumber = '1247415214'
       const status = Status.REGISTRATION_REQUEST_RECEIVED
@@ -120,7 +119,7 @@ describe('Registration request repository', () => {
 
     it('should return false if a registration request is not found, given an non-existent messageId', async () => {
       // given
-      const nonExistentMessageId = uuid();
+      const nonExistentMessageId = 'bcd566db-2044-4dfc-88e7-5487ccb80f7e';
 
       // when
       const foundRecord = await registrationRequestExistsWithMessageId(nonExistentMessageId);
