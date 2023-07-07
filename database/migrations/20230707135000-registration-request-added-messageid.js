30dfc8e6d8dfe95862d16f1baae68bd4caf47628
martin-nhs <martin.taylor22@nhs.net>
2023-07-07 15:08:13 +0100

    [PRMT-2730] Failing pipeline integration test fix.

--- database/migrations/20230707135000-registration-request-added-messageid.js
+++ database/migrations/20230707135000-registration-request-added-messageid.js
@@ -6,9 +6,9 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(transaction => {
       return queryInterface.addColumn(tableName, 'message_id', {
-          type: Sequelize.DataTypes.STRING,
-          allowNull: false,
-          unique: true
+          type: Sequelize.DataTypes.UUID,
+          allowNull: true,
+          unique: false
         }, { transaction });
     });
   },
