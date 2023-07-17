bb6126a3a0f9af6b997d721674ab9f7f3e06136d
Joe Fong <joe.fong1@nhs.net>
2023-07-17 16:48:33 +0100

    [PRMT-3518] Add test for special case of EHR with Large medical history

--- src/services/gp2gp/__tests__/log-outbound-message.nock.test.js
+++ src/services/gp2gp/__tests__/log-outbound-message.nock.test.js
@@ -86,4 +86,38 @@ describe('logOutboundMessage', () => {
       expect(isSmallerThan256KB(outboundEhrMessage)).toBe(false);
     });
   });
+
+  describe('Special test case: EHR with Large medical history which is > 256KB even without the base64 content', () => {
+    it(`should log the whole outbound message in multiple lines of logs, each line to be within 256 KB`, async () => {
+      // given
+      const [conversationId, messageId, ehrRequestId] = createRandomUUID(3);
+      const inputEhrMessage = loadTestData('TestEhrCoreWithLargeMedicalHistory');
+      const odsCode = 'test-ods-code';
+
+      // when
+      const scope = createMockGP2GPScope(EhrMessageType.core);
+      await sendCore(conversationId, odsCode, inputEhrMessage, ehrRequestId, messageId);
+
+      // then
+      expect(scope.isDone()).toBe(true);
+
+      const relevantLogs = logInfo.mock.calls
+        .map(args => args[0])
+        .filter(loggedText => loggedText.match(/Part \d+ of \d+: /));
+
+      // verify that every line of the log is less than 256 KB
+      expect(relevantLogs.length).toBeGreaterThan(0);
+      relevantLogs.forEach(singleLineOfLog => {
+        expect(isSmallerThan256KB(singleLineOfLog)).toBe(true);
+      });
+
+      // verify that we can combine all lines of the log to reconstruct the actual outbound message (with base64 contents removed)
+      const combinedLogs = relevantLogs
+        .map(singleLineOfLog => singleLineOfLog.replace(/Part \d+ of \d+: /, ''))
+        .join('');
+      const restoredMessage = JSON.parse(combinedLogs);
+
+      expect(restoredMessage.coreEhr).toEqual(removeBase64Payloads(inputEhrMessage));
+    });
+  });
 });
