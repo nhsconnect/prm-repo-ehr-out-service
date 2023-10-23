6175487a0f210ff6fea73edf946e26fc3111abbb
martin-nhs <martin.taylor22@nhs.net>
2023-10-23 14:41:03 +0100

    Revert "[PRMT-4074] Address mutability issue regarding `getServiceStartedTimestamp`."
    This reverts commit bfaecc055e738257f3de784b0004f735b2e97ff5.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
 import app from './app';
-import { portNumber } from './config';
+import { portNumber, serviceStartedTimestamp } from './config';
 import { logInfo } from './middleware/logging';
 import { startSqsConsumer } from './services/sqs/sqs-consumer';
-
-let serviceStartedTimestamp;
 
 app.listen(portNumber, () => {
   serviceStartedTimestamp = Date.now();
   logInfo(`The EHR Out Service is now listening on port ${portNumber} for incoming connections.`);
   startSqsConsumer();
 });
-
-export const getServiceStartedTimestamp = () => serviceStartedTimestamp;
