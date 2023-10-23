7693034744b1480d33a58c6847ca9339906f1d79
martin-nhs <martin.taylor22@nhs.net>
2023-10-23 14:53:12 +0100

    [PRMT-4074] Address mutability issue regarding `getServiceStartedTimestamp`.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import app from './app';
-import { portNumber, serviceStartedTimestamp } from './config';
+import { portNumber } from './config';
 import { logInfo } from './middleware/logging';
 import { startSqsConsumer } from './services/sqs/sqs-consumer';
 
 app.listen(portNumber, () => {
-  serviceStartedTimestamp = Date.now();
   logInfo(`The EHR Out Service is now listening on port ${portNumber} for incoming connections.`);
   startSqsConsumer();
 });
