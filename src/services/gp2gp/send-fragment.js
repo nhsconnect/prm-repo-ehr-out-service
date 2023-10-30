ad4bb7d94b3dc4268af4b60ce15e51a517c35ae7
martin-nhs <martin.taylor22@nhs.net>
2023-10-30 14:44:25 +0000

    [PRMT-4074] Tidied up comments.

--- src/services/gp2gp/send-fragment.js
+++ src/services/gp2gp/send-fragment.js
@@ -3,13 +3,12 @@ import { logInfo } from '../../middleware/logging';
 import { config } from '../../config';
 import { FragmentSendingError } from '../../errors/errors';
 import { logOutboundMessage } from './logging-utils';
-import { createFragmentDbRecord } from '../database/create-fragment-db-record';
 import { updateFragmentStatus } from '../transfer/transfer-out-util';
 import { Status } from '../../models/message-fragment';
 import { setCurrentSpanAttributes } from '../../config/tracing';
 
 export const sendFragment = async (conversationId, odsCode, fragmentMessage, messageId) => {
-  const {gp2gpMessengerAuthKeys, gp2gpMessengerServiceUrl} = config();
+  const { gp2gpMessengerAuthKeys, gp2gpMessengerServiceUrl} = config();
   const sendFragmentEndpoint = `${gp2gpMessengerServiceUrl}/ehr-out-transfers/fragment`;
   const requestBody = {conversationId, odsCode, fragmentMessage, messageId};
 
