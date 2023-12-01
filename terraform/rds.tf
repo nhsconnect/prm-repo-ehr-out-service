cbd6e0496692db0a591590025b9c0515616439b5
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2023-12-01 14:21:30 +0000

    [PRMT-4252-RDS] Corrected `backup_retention_period` to 35 and `preferred_backup_window` to 06:30-08:00. (#83)

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -5,21 +5,21 @@ resource "aws_rds_cluster" "ehr_out_service" {
   database_name           = var.db_name
   master_username         = data.aws_ssm_parameter.db-username.value
   master_password         = data.aws_ssm_parameter.db-password.value
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  vpc_security_group_ids  = [
+  backup_retention_period = 35
+  preferred_backup_window = "06:30-08:00"
+  vpc_security_group_ids = [
     aws_security_group.ehr_out_service_db_sg.id,
     aws_security_group.gocd_to_db_sg.id,
     aws_security_group.vpn_to_db_sg.id
   ]
-  apply_immediately         = true
-  db_subnet_group_name      = aws_db_subnet_group.ehr_out_service_db.name
-  final_snapshot_identifier = "${var.component_name}-db-final"
-  storage_encrypted         = true
-  kms_key_id                = aws_kms_key.ehr_out_service_db_key.arn
-  iam_database_authentication_enabled  = true
-  deletion_protection = var.enable_rds_cluster_deletion_protection
-  db_cluster_parameter_group_name = data.aws_ssm_parameter.repo_databases_parameter_group_name.value
+  apply_immediately                   = true
+  db_subnet_group_name                = aws_db_subnet_group.ehr_out_service_db.name
+  final_snapshot_identifier           = "${var.component_name}-db-final"
+  storage_encrypted                   = true
+  kms_key_id                          = aws_kms_key.ehr_out_service_db_key.arn
+  iam_database_authentication_enabled = true
+  deletion_protection                 = var.enable_rds_cluster_deletion_protection
+  db_cluster_parameter_group_name     = data.aws_ssm_parameter.repo_databases_parameter_group_name.value
 
   tags = {
     CreatedBy   = var.repo_name
@@ -34,7 +34,7 @@ resource "aws_rds_cluster" "ehr_out_service" {
 }
 
 resource "aws_ssm_parameter" "db_name" {
-  name =  "/repo/${var.environment}/output/${var.repo_name}/db-name"
+  name  = "/repo/${var.environment}/output/${var.repo_name}/db-name"
   type  = "String"
   value = aws_rds_cluster.ehr_out_service.database_name
 }
@@ -43,7 +43,7 @@ resource "aws_kms_key" "ehr_out_service_db_key" {
 
   description = "${var.component_name} DB KMS key in ${var.environment} environment"
   tags = {
-    Name = "${var.environment}-ehr-out-service-db-key"
+    Name        = "${var.environment}-ehr-out-service-db-key"
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
@@ -59,19 +59,19 @@ resource "aws_db_subnet_group" "ehr_out_service_db" {
   subnet_ids = split(",", data.aws_ssm_parameter.deductions_private_db_subnets.value)
 
   tags = {
-    Name = "${var.environment}-ehr-out-service-db-subnet-group"
+    Name        = "${var.environment}-ehr-out-service-db-subnet-group"
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
 }
 
 resource "aws_rds_cluster_instance" "ehr_out_service_db_instances" {
-  count                 = var.db_instance_number
-  identifier            = "${var.environment}-${var.component_name}-db-instance-${count.index}"
-  cluster_identifier    = aws_rds_cluster.ehr_out_service.id
-  instance_class        = "db.t3.medium"
-  engine                = "aurora-postgresql"
-  db_subnet_group_name  = aws_db_subnet_group.ehr_out_service_db.name
+  count                = var.db_instance_number
+  identifier           = "${var.environment}-${var.component_name}-db-instance-${count.index}"
+  cluster_identifier   = aws_rds_cluster.ehr_out_service.id
+  instance_class       = "db.t3.medium"
+  engine               = "aurora-postgresql"
+  db_subnet_group_name = aws_db_subnet_group.ehr_out_service_db.name
 
   tags = {
     CreatedBy   = var.repo_name
@@ -80,8 +80,8 @@ resource "aws_rds_cluster_instance" "ehr_out_service_db_instances" {
 }
 
 resource "aws_security_group" "ehr_out_service_db_sg" {
-  name        = "${var.environment}-ehr-out-service-db-sg"
-  vpc_id      = data.aws_ssm_parameter.deductions_private_vpc_id.value
+  name   = "${var.environment}-ehr-out-service-db-sg"
+  vpc_id = data.aws_ssm_parameter.deductions_private_vpc_id.value
 
   ingress {
     description     = "Allow traffic from ${var.component_name} to the db"
@@ -92,7 +92,7 @@ resource "aws_security_group" "ehr_out_service_db_sg" {
   }
 
   tags = {
-    Name = "${var.environment}-${var.component_name}-db-sg"
+    Name        = "${var.environment}-${var.component_name}-db-sg"
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
@@ -104,7 +104,7 @@ resource "aws_security_group" "ehr_out_service_db_sg" {
 
 
 resource "aws_security_group" "gocd_to_db_sg" {
-  name = "${var.environment}-gocd-to-${var.component_name}-db-sg"
+  name   = "${var.environment}-gocd-to-${var.component_name}-db-sg"
   vpc_id = data.aws_ssm_parameter.deductions_private_vpc_id.value
 
   ingress {
@@ -116,7 +116,7 @@ resource "aws_security_group" "gocd_to_db_sg" {
   }
 
   tags = {
-    Name = "${var.environment}-gocd-to-${var.component_name}-db-sg"
+    Name        = "${var.environment}-gocd-to-${var.component_name}-db-sg"
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
@@ -127,11 +127,11 @@ resource "aws_security_group" "gocd_to_db_sg" {
 }
 
 resource "aws_security_group" "vpn_to_db_sg" {
-  name = "${var.environment}-vpn-to-${var.component_name}-db-sg"
+  name   = "${var.environment}-vpn-to-${var.component_name}-db-sg"
   vpc_id = data.aws_ssm_parameter.deductions_private_vpc_id.value
 
   tags = {
-    Name = "${var.environment}-vpn-to-${var.component_name}-db-sg"
+    Name        = "${var.environment}-vpn-to-${var.component_name}-db-sg"
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
@@ -142,18 +142,18 @@ resource "aws_security_group" "vpn_to_db_sg" {
 }
 
 resource "aws_security_group_rule" "vpn_to_db_sg" {
-  count       = var.grant_access_through_vpn ? 1 : 0
-  type        = "ingress"
-  description = "Allow traffic from VPN to the db"
-  protocol    = "tcp"
-  from_port   = 5432
-  to_port     = 5432
+  count                    = var.grant_access_through_vpn ? 1 : 0
+  type                     = "ingress"
+  description              = "Allow traffic from VPN to the db"
+  protocol                 = "tcp"
+  from_port                = 5432
+  to_port                  = 5432
   source_security_group_id = data.aws_ssm_parameter.vpn_sg_id.value
-  security_group_id = aws_security_group.vpn_to_db_sg.id
+  security_group_id        = aws_security_group.vpn_to_db_sg.id
 }
 
 resource "aws_ssm_parameter" "db_host" {
-  name =  "/repo/${var.environment}/output/${var.repo_name}/db-host"
+  name  = "/repo/${var.environment}/output/${var.repo_name}/db-host"
   type  = "String"
   value = aws_rds_cluster.ehr_out_service.endpoint
 
@@ -164,7 +164,7 @@ resource "aws_ssm_parameter" "db_host" {
 }
 
 resource "aws_ssm_parameter" "db_resource_cluster_id" {
-  name =  "/repo/${var.environment}/output/${var.repo_name}/db-resource-cluster-id"
+  name  = "/repo/${var.environment}/output/${var.repo_name}/db-resource-cluster-id"
   type  = "String"
   value = aws_rds_cluster.ehr_out_service.cluster_resource_id
 
