5e07c372df1e5d53d7af581eb3be38c19c069401
Joe Fong <joe.fong1@nhs.net>
2024-03-15 09:25:29 +0000

    [PRMT-4587] migrate GET /registration-request endpoint and registration-request-status.test.js

--- src/api/registration-request/registration-request-status.js
+++ src/api/registration-request/registration-request-status.js
@@ -1,5 +1,5 @@
 import { param } from 'express-validator';
-import { getRegistrationRequestByConversationId } from '../../services/database/registration-request-repository';
+import { getOutboundConversationById } from '../../services/database/dynamodb/outbound-conversation-repository';
 import { logError, logInfo } from '../../middleware/logging';
 import { setCurrentSpanAttributes } from '../../config/tracing';
 
@@ -11,14 +11,19 @@ export const registrationRequestStatusValidationRules = [
 export const registrationRequestStatus = async (req, res) => {
   try {
     setCurrentSpanAttributes({ conversationId: req.params.conversationId });
-    const registrationRequest = await getRegistrationRequestByConversationId(req.params.conversationId);
+    const registrationRequest = await getOutboundConversationById(req.params.conversationId);
 
     if (registrationRequest === null) {
       logInfo('Registration not found');
       return res.sendStatus(404);
     }
 
-    const { conversationId, nhsNumber, odsCode, status } = registrationRequest;
+    const {
+      OutboundConversationId: conversationId,
+      NhsNumber: nhsNumber,
+      DestinationGp: odsCode,
+      TransferStatus: status
+    } = registrationRequest;
 
     const data = {
       data: {
