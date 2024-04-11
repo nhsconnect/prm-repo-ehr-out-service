e21f60604486179c023573d1ffb6b647cc7e9e23
martin-nhs <martin.taylor22@nhs.net>
2024-04-11 10:30:52 +0100

    [PRMT-4738] Added `updateCoreStatusInDb` logic to ehr-core-repository.js

--- src/services/database/dynamodb/ehr-core-repository.js
+++ src/services/database/dynamodb/ehr-core-repository.js
@@ -1,6 +1,7 @@
 import { EhrTransferTracker } from './dynamo-ehr-transfer-tracker';
-import { isCore } from '../../../models/core';
+import { buildCoreUpdateParams, isCore } from '../../../models/core';
 import { logInfo } from '../../../middleware/logging';
+import { CoreStatus } from "../../../constants/enums";
 
 export const messageIdMatchOutboundCore = async (outboundConversationId, messageId) => {
   logInfo('Comparing the received messageId with outbound records');
@@ -15,3 +16,27 @@ export const messageIdMatchOutboundCore = async (outboundConversationId, message
 
   return messageId === core.OutboundMessageId;
 };
+
+export const updateCoreStatusInDb = async (
+  inboundConversationId,
+  inboundMessageId,
+  status,
+  failureReason = null
+) => {
+  const database = EhrTransferTracker.getInstance();
+  const updateContent = {
+    TransferStatus: status
+  };
+
+  if (status === CoreStatus.OUTBOUND_FAILED && failureReason) {
+    updateContent.FailureReason = failureReason;
+  }
+
+  const updateParams = buildCoreUpdateParams(
+      inboundConversationId,
+      inboundMessageId,
+      updateContent
+  );
+
+  await database.updateSingleItem(updateParams);
+}
