b530b09521de22b592afb81048a9d22dd7845754
martin-nhs <martin.taylor22@nhs.net>
2024-04-11 10:31:34 +0100

    [PRMT-4738] Added `buildCoreUpdateParams` logic to core.js

--- src/models/core.js
+++ src/models/core.js
@@ -1,5 +1,30 @@
 import { RecordType } from '../constants/enums';
 import { isFragment } from './fragment';
+import { getUKTimestamp } from "../services/time";
+import { addChangesToUpdateParams } from "../utilities/dynamodb-helper";
+import { validateIds } from "../utilities/validation-utilities";
+
+const fieldsAllowedToUpdate = [
+  'TransferStatus',
+  'FailureReason'
+];
+
+export const buildCoreUpdateParams = (inboundConversationId, inboundMessageId, changes) => {
+  validateIds(inboundConversationId, inboundMessageId);
+
+  const params = {
+    Key: {
+      InboundConversationId: inboundConversationId,
+      Layer: RecordType.CORE
+    },
+    UpdateExpression: `set UpdatedAt = :now`,
+    ExpressionAttributeValues: {
+      ':now': getUKTimestamp()
+    }
+  };
+
+  return addChangesToUpdateParams(params, changes, fieldsAllowedToUpdate);
+};
 
 export const isCore = dynamoDbItem => {
   return dynamoDbItem?.Layer?.startsWith(RecordType.CORE);
