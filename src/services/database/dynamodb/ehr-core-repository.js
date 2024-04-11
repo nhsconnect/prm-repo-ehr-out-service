adf10cd505bb5b41f35de3cc7f25fac7770ab0a3
martin-nhs <martin.taylor22@nhs.net>
2024-04-11 10:54:30 +0100

    [PRMT-4738] Removed unnecessary parameters, added logic to fetch with Outbound Conversation ID.

--- src/services/database/dynamodb/ehr-core-repository.js
+++ src/services/database/dynamodb/ehr-core-repository.js
@@ -18,25 +18,35 @@ export const messageIdMatchOutboundCore = async (outboundConversationId, message
 };
 
 export const updateCoreStatusInDb = async (
-  inboundConversationId,
-  inboundMessageId,
+  outboundConversationId,
   status,
   failureReason = null
 ) => {
   const database = EhrTransferTracker.getInstance();
+
+  const coreRecord = await getCoreByOutboundConversationId(outboundConversationId);
+  if (!coreRecord) {
+    throw new Error(`Could not find a CORE record with Outbound Conversation ID ${outboundConversationId}`);
+  }
+
+  const inboundConversationId = coreRecord.InboundConversationId;
   const updateContent = {
     TransferStatus: status
   };
-
   if (status === CoreStatus.OUTBOUND_FAILED && failureReason) {
     updateContent.FailureReason = failureReason;
   }
 
-  const updateParams = buildCoreUpdateParams(
-      inboundConversationId,
-      inboundMessageId,
-      updateContent
-  );
-
+  const updateParams = buildCoreUpdateParams(inboundConversationId, updateContent);
   await database.updateSingleItem(updateParams);
 }
+
+export const getCoreByOutboundConversationId = async outboundConversationId => {
+  const database = EhrTransferTracker.getInstance();
+  const outboundRecords = await database.queryTableByOutboundConversationId(outboundConversationId);
+  const coreRecord = outboundRecords?.filter(isCore)?.[0];
+  if (!coreRecord) {
+    return null;
+  }
+  return coreRecord;
+};
