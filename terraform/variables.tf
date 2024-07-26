5cfb161d26c4ac6d3742ccc39a3ec9979efb9812
Kris Bloe <kris.bloe@answerdigital.com>
2024-07-26 13:24:36 +0100

    [PRMP-132] Removing the ehr-out DB as it will never be used!

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,47 +4,44 @@ variable "region" {
 }
 
 variable "repo_name" {
-  type = string
+  type    = string
   default = "prm-repo-ehr-out-service"
 }
 
-variable "db_name" {
-  type = string
-  default = "ehroutdb"
-}
-
 variable "environment" {}
+
 variable "component_name" {
   default = "ehr-out-service"
 }
+
 variable "task_image_tag" {}
+
 variable "task_cpu" {
   default = 512
 }
+
 variable "task_memory" {
   default = 1024
 }
+
 variable "port" {
   default = 3000
 }
+
 variable "service_desired_count" {
   default = "3"
 }
+
 variable "alb_deregistration_delay" {
   default = 15
 }
-variable "application_database_user" {
-  default = "application_user"
-  description = "Needs to match with the user created in db-roles tf plan"
-}
+
 variable "grant_access_through_vpn" {
   default = false
 }
+
 variable "allow_vpn_to_ecs_tasks" {
   default = false
-}
-variable "enable_rds_cluster_deletion_protection" {
-  default = true
 }
 
 variable "threshold_approx_age_oldest_message" {
@@ -56,10 +53,6 @@ variable "period_of_age_of_message_metric" {
 }
 
 variable "log_level" {
-  type = string
+  type    = string
   default = "info"
-}
-
-variable "db_instance_number" {
-  default = 3
 }
